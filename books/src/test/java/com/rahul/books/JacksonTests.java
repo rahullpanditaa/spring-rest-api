3fe2f8f34d8ad6cc1fb2d99e49a634fa52b85c21
Rahul Pandita <rpandita1999@outlook.com>
2025-05-06 08:10:20 +0530

    testing whether object mapper maps from pojo to json

---
+++ books/src/test/java/com/rahul/books/JacksonTests.java
@@ -0,0 +1,13 @@
+package com.rahul.books;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+public class JacksonTests {
+
+    @Test
+    public void objectMapperJavaObjectToJSON() {
+        ObjectMapper objectMapper = new ObjectMapper(); // jackson object mapper
+
+    }
+}
