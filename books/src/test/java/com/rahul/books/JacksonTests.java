669ebeeabfee2cc16b02c4dc35f5ceca5d998a19
Rahul Pandita <rpandita1999@outlook.com>
2025-05-06 08:24:46 +0530

    test to check jackson object mapper converts from json to Book object

--- books/src/test/java/com/rahul/books/JacksonTests.java
+++ books/src/test/java/com/rahul/books/JacksonTests.java
@@ -18,8 +18,26 @@ public class JacksonTests {
                 .yearPublished("2005")
                 .build();
 
+        // convert (serialize) java object into a JSON string
         String result = objectMapper.writeValueAsString(book);
         assertThat(result).isEqualTo("blah");
+    }
+
+    @Test
+    public void objectMapperJSONStringToJavaObject() throws JsonProcessingException {
+        Book book = Book.builder().isbn("978-0-13-478627-5")
+                .title("The Enigma of Eternity")
+                .author("Aria Montgomery")
+                .yearPublished("2005")
+                .build();
+        String json = "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}";
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        Book result = objectMapper.readValue(json, Book.class);
+
+        assertThat(result).isEqualTo(book);
+
 
     }
 }
