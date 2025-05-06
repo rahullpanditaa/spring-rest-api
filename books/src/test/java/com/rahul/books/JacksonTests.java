af93c0f097290aa9afea37f7687f5bb4481fb364
Rahul Pandita <rpandita1999@outlook.com>
2025-05-06 08:15:36 +0530

    convert Book object into a JSON string; check whether test fails

--- books/src/test/java/com/rahul/books/JacksonTests.java
+++ books/src/test/java/com/rahul/books/JacksonTests.java
@@ -1,13 +1,25 @@
 package com.rahul.books;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rahul.books.domain.Book;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class JacksonTests {
 
     @Test
-    public void objectMapperJavaObjectToJSON() {
+    public void objectMapperJavaObjectToJSON() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper(); // jackson object mapper
+        Book book = Book.builder().isbn("978-0-13-478627-5")
+                .title("The Enigma of Eternity")
+                .author("Aria Montgomery")
+                .yearPublished("2005")
+                .build();
+
+        String result = objectMapper.writeValueAsString(book);
+        assertThat(result).isEqualTo("blah");
 
     }
 }
